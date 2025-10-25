d90ad17ab61b1651fc383a8452db06979b26a7d6
= <=>
2025-10-25 12:11:47 -0300

    feat: user routes implemented

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
+import cors from "cors";
 import express from "express";
 import env from "./env.js";
+import router from "./routes/index.js";
 
 const server = express();
+
+server.use(cors());
+server.use(express.json());
 
 server.listen(env.PORT, () => {
   console.log("Server is running!!");
@@ -10,3 +15,5 @@ server.listen(env.PORT, () => {
 server.get("/", (req, res) => {
   res.send("Hello");
 });
+
+server.use("/user", router.user);
